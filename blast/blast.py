5fec86b6854806096268e95894447d99e6477770
kaymers <70233857+kaymers@users.noreply.github.com>
2020-08-26 16:06:20 +0530

    Update blast.py

--- blast/blast.py
+++ blast/blast.py
@@ -7,7 +7,9 @@ import pandas as pd
 
 
 def process(config, data, input_fasta_path):
-    blast_db = config['blast_db_directory'] + "/" + config['blast_db_name']
+    blast_db = '"' + config['blast_db_directory'].replace('"','') + "/" + config['blast_db_name'].replace('"','') + '"'
+    input_fasta_path = '"'+str(input_fasta_path)+'"'
+    print(blast_db, input_fasta_path)
     blastn_results_xml = NcbiblastnCommandline(query=input_fasta_path, db=blast_db, outfmt=5)()
     blastn_results = NCBIXML.parse(StringIO(blastn_results_xml[0]))
 
