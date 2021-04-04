911a275ea5b6bbfa562cb050cbbf8c7de9db1e6e
sadeepdarshana <darshanasadeep@gmail.com>
2021-04-04 14:26:43 +0530

    Subsample sequences at reader level

--- reader/reader.py
+++ reader/reader.py
@@ -5,6 +5,10 @@ def process(config, path):
     file=SeqIO.parse(path, "fasta")
 
     sequences = []
+    ii=0
     for i in file:
-        sequences.append(str(i.seq))
+        ii+=1
+        if ii%100000==0:print(ii)
+        if ii%100==0:sequences.append(str(i.seq))
+    print(sequences)
     return pd.DataFrame(sequences, columns=['sequence'])
