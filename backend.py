ce47e9a2d29ef2e3f69ff163680a26ec473981db
Kalana Wijegunarathna <kalanainduwara777@gmail.com>
2021-04-12 13:29:27 +0530

    new reader

--- backend.py
+++ backend.py
@@ -1,7 +1,8 @@
 import configparser as cp
 import os
 import sys
-backend_path = os.path.dirname(os.path.realpath(__file__))+"/"
+
+backend_path = os.path.dirname(os.path.realpath(__file__)) + "/"
 sys.path.append(backend_path)
 
 from mapper import mapper_search
@@ -12,28 +13,28 @@ from gc_counter import gc_counter
 from autoencoder import autoencoder
 from reader import reader
 
-def process(input_fasta_path, k, epochs, activation, layers_sizes, train_perc, config_path = backend_path+'/config.ini'):
+
+def process(input_fasta_path, k, epochs, activation, layers_sizes, train_perc, config_path=backend_path + '/config.ini',
+            data=None):
     config = parse_config(config_path)
 
-    data = reader.process(config, input_fasta_path)
+    data = reader.process(config, input_fasta_path, k)
+    print('loaded & vectorized input file')
 
-    print('loaded input file')
-    vectorizer.process(config, data, k)
-    print('vectorizing done')
-    gc_counter.process(config, data)
     print('gc count done')
-    blast.process(config, data, input_fasta_path)
-    print('blast done')
-    mapper_search.process(config, data)
-    print('mapping done')
-    taxonomy.process(config, data)
+    # blast.process(config, data, input_fasta_path)
+    # print('blast done')
+    # mapper_search.process(config, data)
+    # print('mapping done')
+    # taxonomy.process(config, data)
     autoencoder.process(config, data, epochs, activation, layers_sizes, train_perc)
-    data.drop('sequence', axis=1, inplace=True)
-    data.drop('vector', axis=1, inplace=True)
+    # data.drop('sequence', axis=1, inplace=True)
+    # data.drop('vector', axis=1, inplace=True)
     return data
 
 
-def process_ae(input_fasta_path, k, epochs, activation, layers_sizes, train_perc, data, config_path = backend_path+'/config.ini'):
+def process_ae(input_fasta_path, k, epochs, activation, layers_sizes, train_perc, data,
+               config_path=backend_path + '/config.ini'):
     config = parse_config(config_path)
 
     sequences = reader.process(config, input_fasta_path)
@@ -54,4 +55,3 @@ def parse_config(config_path):
     for key in [i[0] for i in config.items()]:
         config[key] = config[key].replace("{BACKEND_PATH}", backend_path)
     return config
-
